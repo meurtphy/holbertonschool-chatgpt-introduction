3feb29b20249bb66808f828c28922c31221e2ee1
meurtphy <erwanmpn5@gmail.com>
2025-01-16 09:26:34 +0100

    espace

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,7 +5,7 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Decrement n to avoid infinite loop
+        n -= 1
     return result
 
 if len(sys.argv) != 2:
